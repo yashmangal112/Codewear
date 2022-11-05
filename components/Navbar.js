4161590f4a9108c6a8eb631c81b4d4aaca343a81
Yash <=>
2022-11-05 14:20:30 +0530

    upto date

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { GiShoppingBag } from 'react-icons/gi';
 import { IoIosCloseCircle } from 'react-icons/io';
+import { BsFillBagCheckFill } from 'react-icons/bs';
+import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
 import { useRef } from 'react';
 
 const Navbar = () => {
@@ -18,6 +20,17 @@ const Navbar = () => {
   }
 
   const ref = useRef()
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log(`You clicked ${count} times`) 
+    if (count<1) {
+      console.log("clearCart function use");
+    }
+  });
+
+
+  const [count, setCount] = useState(1);
   return (
     <div className='flex flex-col md:justify-start md:flex-row items-center justify-between py-2 shadow-xl'>
       <div className="logo mx-5">
@@ -35,17 +48,30 @@ const Navbar = () => {
       <div onClick={toggleCart} className="cursor-pointer  hover:text-indigo-900 cart right-0 top-4 absolute mx-5">
         <GiShoppingBag className= "text-xl md:text-2xl"/>
       </div>
-      <div ref={ref} className="w-72 sideCart absolute top-0 right-0 bg-indigo-100 py-10 px-8 transform transition-transform translate-x-full">
+      <div ref={ref} className="w-72 h-full sideCart z-10 absolute top-0 right-0 bg-indigo-100 py-10 px-8 transform transition-transform translate-x-full">
         <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
         <span onClick={toggleCart} className="absolute top-5 right-3 cursor-pointer hover:text-indigo-600 text-2xl"><IoIosCloseCircle/></span>
         <ol className='list-decimal font-semibold'>
+
+          
+          {/* Cart name */}
           <li>
             <div className="item flex my-5">
               <div className='w-2/3 font-semibold'>Tshirts - Wear the code</div>
-              <div className='flex w-1/3 font-semibold items-center justify-center'>1</div>
+              <div className='flex w-1/3 font-semibold items-center justify-center text-lg'>
+                <AiFillMinusCircle onClick={()=> setCount(count-1)} className='cursor-pointer text-indigo-500'/>
+                <span className='mx-2 text-sm'>{count}</span>
+                <AiFillPlusCircle onClick={()=> setCount(count+1)} className='cursor-pointer text-indigo-500'/>
+              </div>
             </div>
           </li>
+
         </ol>
+        <div className="flex">
+        <button class="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm"><BsFillBagCheckFill className='m-1'/>Checkout</button>
+        <button class="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">ClearCart</button>
+
+        </div>
       </div>
     </div>
   )
